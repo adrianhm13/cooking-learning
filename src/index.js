b0a22bc9248c30910c5b00de5f39131509755e50
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-12-06 17:41:05 +0100

    Learning use of useReducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "../src/context/ThemeContext";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
